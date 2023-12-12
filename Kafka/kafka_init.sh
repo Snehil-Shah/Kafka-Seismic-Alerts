14a677ca4c240325cdb77bbc56ca5df03075979c
Snehil Shah <snehilshah.989@gmail.com>
2023-12-13 02:58:32 +0530

    Add Kafka initialization scripts & Docker configurations

---
+++ Kafka/kafka_init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Start the Kafka server
+../../../etc/confluent/docker/run &
+
+# # Create the topics
+../../bin/kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic minor_seismic_events
+../../bin/kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic severe_seismic_events
+
+# # Keep the script running
+tail -f /dev/null
