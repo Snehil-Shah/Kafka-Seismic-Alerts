edaad75d4dfc49b4e7c4d98e9b557032a2f7cb69
Snehil Shah <snehilshah.989@gmail.com>
2024-02-17 23:07:42 +0530

    Fix Kafka-JDBC Connector Not Receiving Infinite Connection Attempts
    Signed-off-by: Snehil Shah <snehilshah.989@gmail.com>

--- Consumers/src/main/java/kafka/consumers/clients/Database.java
+++ Consumers/src/main/java/kafka/consumers/clients/Database.java
@@ -4,39 +4,51 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
 
+/**
+ * Datastore Consumer Class:
+ * Initializes Kafka-JDBC Connection
+ */
+
 public class Database {
+
+    /**
+     * Initializes Database Connection to Kafka Stream as a Sink
+     */
     public void kafka_connect() {
         try {
-            URI connector_uri = new URI("http://kafka-db-connector:8083/connectors");
-            HttpURLConnection connection = (HttpURLConnection) connector_uri.toURL().openConnection();
-            connection.setDoOutput(true);
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json");
+            URI connector_uri = new URI("http://kafka-db-connector:8083/connectors"); // API
             String kafka_connector_config = "{\n" +
                     "    \"name\": \"Logs-Sink\",\n" +
                     "    \"config\": {\n" +
                     "      \"connector.class\": \"io.confluent.connect.jdbc.JdbcSinkConnector\",\n" +
                     "      \"tasks.max\": \"1\",\n" +
                     "      \"topics\": \"severe_seismic_events,minor_seismic_events\",\n" +
-                    "      \"connection.url\": \"jdbc:postgresql://db:5432/Logs\",\n" +
-                    "      \"connection.user\": \"postgres\",\n" +
-                    "      \"connection.password\": \"password\",\n" +
+                    "      \"connection.url\": \"" + System.getenv("KAFKA_JDBC_CONNECTION_URL") + "\",\n" +
+                    "      \"connection.user\": \"" + System.getenv("DB_USERNAME") + "\",\n" +
+                    "      \"connection.password\": \"" + System.getenv("DB_PASSWORD") + "\",\n" +
                     "      \"auto.create\": \"true\"\n" +
                     "    }\n" +
                     "}";
+
             while (true) {
                 try {
+                    HttpURLConnection connection = (HttpURLConnection) connector_uri.toURL().openConnection();
+
+                    connection.setDoOutput(true);
+                    connection.setRequestMethod("POST");
+                    connection.setRequestProperty("Content-Type", "application/json");
+
+                    // Send Kafka-JDBC Connector Config in POST Request
                     try (OutputStream request_stream = connection.getOutputStream()) {
                         byte[] body = kafka_connector_config.getBytes("utf-8");
                         request_stream.write(body, 0, body.length);
                     }
                     if ("Created".equals(connection.getResponseMessage())) {
                         System.out.println("-> Database initialized..");
-                        System.out.println("-> Alerts Enabled..");
                         connection.disconnect();
                         break;
-                    }
-                    else{
+                    } else {
+                        connection.disconnect();
                         Thread.sleep(5000);
                     }
                 } catch (Exception e) {
